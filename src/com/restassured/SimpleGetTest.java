5d969876a1b251ad014a1525607a3954674ef4b6
Mushfiqur Rahman <mushfiq.aiub142@hotmail.com>
2018-08-29 16:47:06 +0600

    assert, header name and content types are added

--- src/com/restassured/SimpleGetTest.java
+++ src/com/restassured/SimpleGetTest.java
@@ -1,9 +1,12 @@
 package com.restassured;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SimpleGetTest {
@@ -13,20 +16,43 @@ public class SimpleGetTest {
         // Specify the base URL to the RESTful web service
         RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
 
-        // Get the RequestSpecification of the request that you want to sent
-        // to the server. The server is specified by the BaseURI that we have
-        // specified in the above step.
+        // Get the RequestSpecification of the request that we want to send
+        // to the server.
         RequestSpecification httpRequest = RestAssured.given();
 
         // Make a request to the server by specifying the method Type and the method URL.
-        // This will return the Response from the server. Store the response in a variable.
+
         Response response = httpRequest.request(Method.GET, "/Dhaka");
 
-        // Now let us print the body of the message to see what response
-        // we have recieved from the server
+        //Header name, Content type
+        String contentType = response.header("Content-Type");
+        System.out.println("Content-Type value: " + contentType);
+
+        //Header name, server type
+        String serverType = response.header("Server");
+        System.out.println("Server value: " + serverType);
+
+        //Header name, content encoding
+        String acceptLanguage = response.header("Content-Encoding");
+        System.out.println("Content-Encoding value: " + acceptLanguage);
+
+        //Get all headers
+        Headers allHeader = response.headers();
+
+        //Iterate over all the headers
+        for (Header header : allHeader) {
+            System.out.println("Key: " + header.getName() + " Value: " + header.getValue());
+        }
+
+        //Get the status line and code
+        int statusCode = response.getStatusCode();
+        String statusLine = response.getStatusLine();
+
+        //Assert the returned value
+        Assert.assertEquals(statusCode, 200, "Correct status code returned");
+
 //        String responseBody = response.getBody().asString();
-        System.out.println("Response Body is => " + response.asString());
+        System.out.println("Status Line is: " + statusLine + "\n Response Body is => " + response.asString());
 
     }
-
 }
